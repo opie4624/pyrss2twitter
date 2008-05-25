403ce633e0c2267b0737a05c8ad55cd1a73022fd
Andrew Kraut <akraut@gmail.com>
2008-05-25 13:40:52 -0500

    Class init completed

--- rss2twit.py
+++ rss2twit.py
@@ -1,69 +1,52 @@
 #!/usr/bin/env python
 
 # python rss reader -> twitter post
-import feedparser, pickle, os, sys, twitter, urllib 
+import feedparser, pickle, os, sys, twitter, urllib, urllib2, hashlib
 
-class RSS2Twitter:
- def __init__(self, filename, url, username, passwd):
- self.filename=filename
- self.url=url
- self.username=username
- self.passwd=passwd
- self.twApi=twitter.Api(username=self.username, password=self.passwd)
+class rss2twit:
+	def __init__(self, feedurl, username, password, filepath = './', feedtag = "", debug=False):
+		self.filepath = filepath
+		self.feedurl = feedurl
+		self.twit = twitter.Api()
+		self.debug = debug
+		
+		self.twit.SetCredentials(username, password)	
+		if (os.path.isdir(self.filepath)):
+			self.filepath = os.path.join(self.filepath, hashlib.sha1(self.feedurl).hexdigest())
+		if os.path.exists(self.filepath):
+			self.itemcache = pickle.load(file(self.filepath, 'r+b'))
+		else:
+			self.itemcache = {} 
+	
+# TODO 
+	def getLatestFeedItems(self, itemLimit = 0):
+		pass
+#		feed=feedparser.parse(self.feedurl);
+#		it=feed["items"]
+#		if(itemLimit > 0):
+#			it=it[0:itemLimit]
+#		return it
 
- if os.path.exists(self.filename):
- self.itemsDB = pickle.load(file(filename, 'r+b'))
- else:
- self.itemsDB = {} 
+# TODO 
+	def twitIt(self, items):
+		pass
+#		pItems=0
+#		for it in items:
+#			if self.itemPublished(it) == None:
+#				print "----\n"
+#				txt=it["title"] +" "+self.tiny(it["link"])
+#				print txt
+#				status = self.twApi.PostUpdate(txt)
+#				print "status: ", status.text
+#				pItems=pItems+1
+#		print "Total items: ", len(items)
+#		print "published: ",pItems
+#		print "old stuff: ",len(items) - pItems
 
- def getLatestFeedItems(self, items = 5):
- feed=feedparser.parse(self.url);
- it=feed["items"]
- it_ret=it[0:items]
- return it_ret
+# TODO
+	def itemPublished (self, item):
+		pass
 
- def twitIt(self, items):
- oldItems=pItems=0
- for it in items:
- if self.itemPublished(it) == None:
- print "————–\n"
- txt=it["title"] +" "+self.tiny(it["link"])
- print txt
- status = self.twApi.PostUpdate(txt)
- print "status: ", status.text
- pItems=pItems+1
- print "Total items: ", len(items)
- print "published: ",pItems
- print "old stuff: ",len(items) - pItems
-
- def itemPublished (self, item):
- if self.itemsDB.has_key(item["link"]) == True:
- return True
- else:
- self.itemsDB[item["link"]]=item["title"]
- pickle.dump(self.itemsDB, file(self.filename, 'w+b'))
- return None
-
- def tiny(self, url):
- try:
-
- data = urllib.urlencode(dict(url=url, source="RSS2Twit"))
- encodedurl="http://www.tinyurl.com/api-create.php?"+data
- instream=urllib.urlopen(encodedurl)
- ret=instream.read()
- instream.close()
-
- if len(ret)==0:
- return url
-
- return ret
- except IOError, e:
- raise "urllib error."
-
-if __name__ == "__main__":
- # run it like python rss2twitter.py oi.dat (oi.dat is the posted item db)
- # update username and passwd with your twitter account data, surrounding them with quotes.
- url="http://rssfeed"
- r2t=RSS2Twitter(sys.argv[1], url, USERNAME, PASSWD)
- its=r2t.getLatestFeedItems()
- r2t.twitIt(its)
+# TODO 
+	def tiny(self, url):
+ 		pass
