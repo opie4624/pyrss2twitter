1ecce18779d2f0656cd12918aad6fb54df234dc9
Andrew Kraut <akraut@gmail.com>
2008-05-26 01:28:56 -0500

    Added Direct Message Processing

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python
 
 import rss2twit
+import twitter
+import os
+import pickle
+
+if os.path.exists('userlist'):
+        userdb = pickle.load(file('userlist', 'r+b'))
+else:
+        userdb = {} 
 
 forums = rss2twit.rss2twit('http://uhacc.org/forums/forum-rss.php', 'uhacc', 'oz8(Kaki', feedtag='New Forums Post', debug=True)
 sysblogd = rss2twit.rss2twit('http://www.uhacc.org/gl_sysblogd/backend/sysblogd.rss', 'uhacc', 'oz8(Kaki', feedtag='', debug=True)
@@ -9,3 +17,18 @@ sysblogd = rss2twit.rss2twit('http://www.uhacc.org/gl_sysblogd/backend/sysblogd.
 forums.go()
 sysblogd.go()
 #talkshoe.go()
+
+twit = twitter.Api()
+twit.SetCredentials('uhacc', 'oz8(Kaki')
+
+gd = twit.GetDirectMessages()
+
+for m in gd:
+	if userdb[m.sender_screen_name] == 'Banned':
+		twit.DestroyDirectMessage(m.id)
+	elif userdb[m.sender_screen_name] == 'Passive Member':
+		twit.PostDirectMessage(m.sender_screen_name, 'Sorry, only Full Members can make announcements.  http://twurl.nl/hqlm5t for membership details.')
+		twit.DestroyDirectMessage(m.id)
+	else:
+		twit.PostUpdate(userdb[m.sender_screen_name]+" @"+m.sender_screen_name+" says: " + forums.blurb(m.text, 9+len(userdb[m.sender_screen_name])+len(m.sender_screen_name)))
+		twit.DestroyDirectMessage(m.id)
